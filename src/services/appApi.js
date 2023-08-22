01df1ceba31f4f0b12da6f2f2187f6a1c0d0a560
MUKUL . <panchalmukul99@gmail.com>
2023-08-22 23:20:13 +0530

    backend link added

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 const appApi=createApi({
     reducerPath: 'appApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001",
+        baseUrl: "https://chat-backend-c5mb.onrender.com",
     }),
 
     endpoints: (builder)=>({
